7fda8de45f5bdf5a6e75fcf3394b31286586f878
Ethan Harvey <ethan.harvey@tufts.edu>
2025-01-15 14:40:20 -0500

    Added toy datasets

--- src/utils.py
+++ src/utils.py
@@ -309,53 +309,6 @@ def use_posterior(self, flag):
             layers.VariationalCNBlock,
         )):
             child.use_posterior = flag
-            
-def _check_bn(module, flag):
-    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
-        flag[0] = True
-
-def check_bn(model):
-    flag = [False]
-    model.apply(lambda module: _check_bn(module, flag))
-    return flag[0]
-
-def reset_bn(module):
-    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
-        module.running_mean = torch.zeros_like(module.running_mean)
-        module.running_var = torch.ones_like(module.running_var)
-
-def _get_momenta(module, momenta):
-    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
-        momenta[module] = module.momentum
-
-def _set_momenta(module, momenta):
-    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
-        module.momentum = momenta[module]
-        
-def bn_update(model, dataloader):
-    if not check_bn(model):
-        return
-    model.train()
-    momenta = {}
-    model.apply(reset_bn)
-    model.apply(lambda module: _get_momenta(module, momenta))
-    n = 0
-    num_batches = len(dataloader)
-
-    with torch.no_grad():
-        for images, _ in dataloader:
-            images = images.cuda(non_blocking=True)
-            images_var = torch.autograd.Variable(images)
-            b = images_var.data.size(0)
-
-            momentum = b / (n + b)
-            for module in momenta.keys():
-                module.momentum = momentum
-
-            model(images_var)
-            n += b
-
-    model.apply(lambda module: _set_momenta(module, momenta))
     
 def flatten_params(model, excluded_params=['sigma_param']):
     return torch.cat([param.view(-1) for name, param in model.named_parameters() if param.requires_grad and name not in excluded_params])
